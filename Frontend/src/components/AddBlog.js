42bd624704318e82673aad8f48fec219c9bc8fae
Yousuf Zubaer <yousufzubaer1622@gmail.com>
2023-07-21 15:07:20 +0600

    useReducer

--- Frontend/src/components/AddBlog.js
+++ Frontend/src/components/AddBlog.js
@@ -1,6 +1,6 @@
 import { Box, Button, InputLabel, TextField, Typography } from '@mui/material'
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { useNavigate } from 'react-router-dom'
 //test1
 
